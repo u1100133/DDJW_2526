8ceb633427e7fd1e7ef5984979c2bae5d91f0c87
Pol Pastor <polpfajula@gmail.com>
2026-04-25 15:35:18 +0200

    afegit els  nous paràmetres a la pantalla d'opcions

--- js/options.js
+++ js/options.js
@@ -1,42 +1,50 @@
 import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 
-var options = function(){
+var options = function() {
     const default_options = {
         pairs: 2,
-        difficulty: 'normal'
+        difficulty: 'normal',
+        groupSize: 2,
+        startLevel: 1
     } 
 
     var pairs = $('#pairs');
     var difficulty = $('#dif');
+    var groupSize = $('#groupSize');
+    var startLevel = $('#startLevel');
     
     var savedOptions = localStorage.options && JSON.parse(localStorage.options);
-    var options = Object.create(default_options);
+    var currentOptions = Object.create(default_options);
 
-    if (savedOptions && savedOptions.pairs)
-        options.pairs = savedOptions.pairs;
-    if (savedOptions && savedOptions.difficulty)
-        options.difficulty = savedOptions.difficulty;
+    if (savedOptions) { //Carreguem opcions si ja existeixen
+        if (savedOptions.pairs) currentOptions.pairs = savedOptions.pairs;
+        if (savedOptions.difficulty) currentOptions.difficulty = savedOptions.difficulty;
+        if (savedOptions.groupSize) currentOptions.groupSize = savedOptions.groupSize;
+        if (savedOptions.startLevel) currentOptions.startLevel = savedOptions.startLevel;
+    }
+    pairs.val(currentOptions.pairs);
+    difficulty.val(currentOptions.difficulty);
+    groupSize.val(currentOptions.groupSize);
+    startLevel.val(currentOptions.startLevel);
 
-    pairs.val(options.pairs);
-    difficulty.val(options.difficulty);
-
-    pairs.on('change', function (){
-        options.pairs = pairs.val();
-    });
-
-    difficulty.on('change', function (){
-        options.difficulty = difficulty.val();
-    });
+    pairs.on('change', function(){ currentOptions.pairs = pairs.val(); });
+    difficulty.on('change', function(){ currentOptions.difficulty = difficulty.val(); });
+    groupSize.on('change', function(){ currentOptions.groupSize = groupSize.val(); }); 
+    startLevel.on('change', function(){ currentOptions.startLevel = startLevel.val(); });
 
     return {
         applyChanges: function(){
-            localStorage.options = JSON.stringify(options);
+            localStorage.options = JSON.stringify(currentOptions);
         },
         defaultValues: function(){
-            options.pairs = default_options.pairs;
-            options.difficulty = default_options.difficulty;
-            pairs.val(options.pairs);
-            difficulty.val(options.difficulty);
+            currentOptions.pairs = default_options.pairs;
+            currentOptions.difficulty = default_options.difficulty;
+            currentOptions.groupSize = default_options.groupSize;
+            currentOptions.startLevel = default_options.startLevel;
+            pairs.val(currentOptions.pairs);
+            difficulty.val(currentOptions.difficulty);
+            groupSize.val(currentOptions.groupSize);
+            startLevel.val(currentOptions.startLevel);
         }
     }
 }();
